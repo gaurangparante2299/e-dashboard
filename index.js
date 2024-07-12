848089ae44ad626aff400dbf2a8d1cf86c1d21df
Gaurang Parante <gaurang.parante@strausshealthcare.com>
2024-07-12 17:55:58 +0530

    Done

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
 const express = require('express');
+// For logs of api call
+// const fs = require('fs');
+const cors = require('cors');
+require('./DB/config');
+const User = require('./DB/user');
 const app = express();
 
-app.get('/', (req, resp) => {
-    resp.send("App is started")
+app.use(express.json());
+app.use(cors());
+
+// For logs of api call
+// var logger = require('morgan');
+// app.use(logger('common', {
+//     stream: fs.createWriteStream('./access.log', { flags: 'a' })
+// }));
+// app.use(logger('dev'));
+
+app.post('/register', async (req, resp) => {
+    let user = new User(req.body);
+    let result = await user.save();
+    result = result.toObject();
+    delete result.password;
+    resp.send(result);
+});
+
+app.post('/login', async (req, resp) => {
+    let user = await User.findOne(req.body).select('-password');
+    // console.log(req.body);
+    if (user) {
+        resp.send(user);
+    } else {
+        resp.send({
+            result: "No user found!!!"
+        })
+    }
 })
 
 app.listen(5000);
